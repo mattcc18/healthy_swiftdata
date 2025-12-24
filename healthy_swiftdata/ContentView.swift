c63aad33a6fd3251521322537e3b91f8db4b40f4
mattcc18 <corcoran.matthew18@gmail.com>
2025-12-24 22:34:17 +0000

    Initial Commit

---
+++ healthy_swiftdata/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  healthy_swiftdata
+//
+//  Created by Matthew Corcoran on 24/12/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ healthy_swiftdata/healthy_swiftdataApp.swift
@@ -0,0 +1,17 @@
+//
+//  healthy_swiftdataApp.swift
+//  healthy_swiftdata
+//
+//  Created by Matthew Corcoran on 24/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct healthy_swiftdataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
